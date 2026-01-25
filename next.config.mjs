d18bed5241706209af00eed90b667fd9e5d7d852
Harlahbest2003 <alabizakariyya22@gmail.com>
2026-01-25 07:07:13 +0100

    doing

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,20 @@ const nextConfig = {
   // Image optimization
   images: {
     formats: ["image/avif", "image/webp"],
-    domains: ["images.unsplash.com", "images.pexels.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
